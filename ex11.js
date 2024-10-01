d1bcb59dc6d2b02426d0ba67972d9c04bdc1d511
Julia_Chu <z3211232001@gmail.com>
2024-10-01 11:53:53 -0700

    completed 11

--- ex11.js
+++ ex11.js
@@ -2,4 +2,18 @@
 // and returns the number of times the word appears in the string.
 // Example: countWordOccurrences("hello world hello", "hello") should return 2.
 
+const countWordOccurrences = function (string, word) {
+      const stringArr = string.split(" ")
+      console.log(stringArr)
+      let results = 0
+      
+      for (let i = 0; i < stringArr.length; i++) {
+            if ( stringArr[i] === word ) {
+                  results = results + 1
+            }
+      }
+      return results
+      
+}
+
 console.log(countWordOccurrences("hello world hello", "hello")); // Expected output: 2
