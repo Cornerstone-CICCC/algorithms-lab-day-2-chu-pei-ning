5b85c01d1dabd1e82a9dee4e9dda7423410b6e1f
Julia_Chu <z3211232001@gmail.com>
2024-10-01 11:53:28 -0700

    completed 05

--- ex05.js
+++ ex05.js
@@ -2,4 +2,16 @@
 // and returns the number of consonants in the string.
 // Example: countConsonants("hello world") should return 7.
 
+const countConsonants = function (str) {
+
+      const noVowels = str.replace(/[aeiou]/gi, '')
+      const noSpace = noVowels.split(" ").join("")
+      const count = noSpace.length
+
+      return count
+
+
+}
+
+
 console.log(countConsonants("hello world")); // Expected output: 7
