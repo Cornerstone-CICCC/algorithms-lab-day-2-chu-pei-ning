3ca830a0ab8fb2878a406de7aafed4e5cf04be92
Julia_Chu <z3211232001@gmail.com>
2024-10-01 11:54:21 -0700

    completed 15

--- ex15.js
+++ ex15.js
@@ -2,4 +2,15 @@
 // and returns the maximum number in the array.
 // Example: findMax([3, 1, 4, 1, 5, 9]) should return 9.
 
+const findMax = function (arr) {
+      const getMax = arr.reduce((max, current ) => {
+            if (current > max) {
+                  return current
+            } else {
+                  return max
+            }
+      })
+      return getMax
+}
+
 console.log(findMax([3, 1, 4, 1, 5, 9])); // Expected output: 9
