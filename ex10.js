fd9aaf3c7a2940b9f16b7bbe6734388181516b96
Julia_Chu <z3211232001@gmail.com>
2024-10-01 12:06:52 -0700

    completed 10

--- ex10.js
+++ ex10.js
@@ -2,4 +2,27 @@
 // with words separated by spaces or underscores, and returns the string in camelCase format.
 // Example: convertToCamelCase("hello_world") should return "helloWorld".
 
+const convertToCamelCase = function(messege){
+      const messegeToArr = messege.split("")
+      let messegeArr = []
+      for (let i = 0; i < messegeToArr.length; i++) {
+            if (messegeToArr.includes("-")) {
+                  messegeArr = messege.split("-")
+            }
+            if (messegeToArr.includes("_")) {
+                  messegeArr = messege.split("_")
+            }
+      }
+      console.log(messegeArr)
+      let newMessege = [messegeArr[0]]
+      for (let i = 1; i < messegeArr.length; i++){
+            newMessege.push(messegeArr[i].charAt(0).toUpperCase() + messegeArr[i].slice(1))
+            
+      }
+      return newMessege.join("")
+}
+
+
+
+
 console.log(convertToCamelCase("hello_world")); // Expected output: "helloWorld"
