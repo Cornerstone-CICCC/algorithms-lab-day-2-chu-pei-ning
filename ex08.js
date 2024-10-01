385201908a3a8ea84ca1cc726329ce8b2831c4ef
Julia_Chu <z3211232001@gmail.com>
2024-10-01 11:53:45 -0700

    completed 08

--- ex08.js
+++ ex08.js
@@ -2,5 +2,21 @@
 // and groups them by age into an object where the keys are ages and the values are arrays of people of that age.
 // Example: groupByAge([{ name: "Alice", age: 25 }, { name: "Bob", age: 25 }, { name: "Charlie", age: 30 }])
 // should return { 25: [{ name: "Alice", age: 25 }, { name: "Bob", age: 25 }], 30: [{ name: "Charlie", age: 30 }] }.
+const groupByAge = function ( arr ) {
+
+      const outputObj = {}
+      for (let i = 0; i < arr.length; i++){
+            let age = arr[i].age
+            if ( age in outputObj ) {
+                  outputObj[age].push(arr[i])
+            } else {
+                  outputObj[age] = [arr[i]]
+            }
+            }
+      return outputObj
+}
+
+
+
 
 console.log(groupByAge([{ name: "Alice", age: 25 }, { name: "Bob", age: 25 }, { name: "Charlie", age: 30 }])); // Expected output: { 25: [{ name: "Alice", age: 25 }, { name: "Bob", age: 25 }], 30: [{ name: "Charlie", age: 30 }] }
