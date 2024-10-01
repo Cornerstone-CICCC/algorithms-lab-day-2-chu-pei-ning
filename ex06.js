df0f94a05b6f4357175d4afad273ea6cfe798c9c
Julia_Chu <z3211232001@gmail.com>
2024-10-01 12:23:40 -0700

    completed 06

--- ex06.js
+++ ex06.js
@@ -3,4 +3,18 @@
 // Example: getQueryParams("https://example.com?search=test&page=2")
 // should return { search: "test", page: "2" }.
 
+const getQueryParams = function ( url ) {
+
+      const splitUrl = url.split("?")
+      const needUrl = splitUrl[1].split("&")
+
+      let useNeedUrl = []
+      for (let i = 0; i < needUrl.length; i++){
+            useNeedUrl.push(needUrl[i].split("="))
+      }
+
+      return Object.fromEntries(useNeedUrl)
+
+}
+
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }
