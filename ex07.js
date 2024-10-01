e69811a87e8baa8424e776437eb1926e2971ad50
Julia_Chu <z3211232001@gmail.com>
2024-10-01 11:53:38 -0700

    completed 07

--- ex07.js
+++ ex07.js
@@ -3,5 +3,18 @@
 // (e.g., the presence of "@" and a ".").
 // Example: validateEmail("test@example.com") should return true.
 
+const validateEmail = function (email) {
+
+      emailArr = email.split("")
+      for (let i = 0; i < emailArr.length; i++) {
+            if ( emailArr.includes("@" || ".") ) {
+                  return true
+            }
+            return false
+      }
+
+}
+
+
 console.log(validateEmail("test@example.com")); // Expected output: true
 console.log(validateEmail("invalid-email.com")); // Expected output: false
