6f2173d5c96587035d583d5216434a462f0ca456
Julia_Chu <z3211232001@gmail.com>
2024-10-01 14:18:47 -0700

    completed 09

--- ex09.js
+++ ex09.js
@@ -3,4 +3,14 @@
 // Example: sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")
 // should return [{name: "Bob", age: 25}, {name: "Alice", age: 30}].
 
+const sortByProperty = function (arr, property) {
+
+      if (property === "name") {
+            return arr.sort((a, b) => a.name.toLowerCase().localeCompare( b.name.toLowerCase()))
+      }
+      if (property === "age") {
+            return arr.sort((a, b) => a.age - b.age)
+      }
+}
+
 console.log(sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")); // Expected output: [{name: "Bob", age: 25}, {name: "Alice", age: 30}]
